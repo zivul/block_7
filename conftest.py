671faa121a1ae87fbfefed1d7f534ecf3b8444ac
zivul <analiz-k@mail.ru>
2024-08-20 15:10:33 +0300

    Block_8

--- conftest.py
+++ conftest.py
@@ -1,8 +1,26 @@
+import logging
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from time import sleep
 from playwright.sync_api import sync_playwright, Playwright
+from selenium.webdriver.common.by import By
+import allure
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+logging.basicConfig(level=logging.DEBUG, filename='my_log.log',
+                    format='%(levelname)s (%(asctime)s): %(message)s (Line: %(lineno)d) [%(filename)s]',
+                    datefmt='%d/%m/%Y %I:%M:%S', encoding='utf-8', filemode='w')
+
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler('test.log', encoding='utf-8')
+formatter = logging.Formatter('%(levelname)s (%(asctime)s): %(message)s (Line: %(lineno)d) [%(filename)s]')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.info('Тестируем файл на данные')
+
 
 
 @pytest.fixture(scope='class')
@@ -16,12 +34,13 @@ def browser(playwright: Playwright):
     browser.close()
 
 
-
 @pytest.fixture()
 def set_up_browser():
     options = ChromeOptions()
     driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(60)
+    driver.maximize_window()
     yield driver
-    sleep(5)
     driver.quit()
+
 
